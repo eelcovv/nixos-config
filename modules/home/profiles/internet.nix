42da1c82c2e91ea56768de8bd3de3a10e3fd91bb
Eelco <eelcovv@gmail.com>
2025-01-06 20:45:04 +0100

    removed erroneous package

--- modules/home/profiles/internet.nix
+++ modules/home/profiles/internet.nix
@@ -8,7 +8,6 @@
     qutebrowser
     tor-browser
     protonmail-desktop
-    BiglyBT # torrent client
     transmission_4 # torrent client -browser
     protonmail-desktop
     biglybt # torrent client
