9cdd704aadc55dcf148971a2b43d57f3ac0f3e4d
Eelco <eelcovv@gmail.com>
2025-01-06 02:47:54 -0500

    changed timezone

--- modules/nixos/linux/current-location.nix
+++ modules/nixos/linux/current-location.nix
@@ -1,9 +1,9 @@
 {
-  time.timeZone = "America/New_York";
+  time.timeZone = "Europe/Amsterdam";
 
   location = {
-    # Quebec City
-    latitude = 46.813359;
-    longitude = -71.215796;
+    # Amsterdam
+    latitude = 52.37;
+    longitude = 4.88;
   };
 }
