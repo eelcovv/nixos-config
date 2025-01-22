f0a9a60d8019dc4675c2ea13a5dd586334aae321
Eelco <eelcovv@gmail.com>
2025-01-22 09:31:49 +0100

    fixed wezterm

--- modules/home/all/wezterm/default.nix
+++ modules/home/all/wezterm/default.nix
@@ -2,8 +2,6 @@
   programs.wezterm = {
     enable = true;
     # https://alexplescan.com/posts/2024/08/10/wezterm/
-    extraConfig = builtins.readFile ./config/wezterm/wezterm.lua;
-    enableZshIntegration = true;
-    enableBashIntegration = true;
+    extraConfig = builtins.readFile ./wezterm.lua;
   };
 }
