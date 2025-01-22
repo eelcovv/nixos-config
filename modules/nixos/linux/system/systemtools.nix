7e9428b6a770bcec72c66bd97bc04a953df93f69
Eelco <eelcovv@gmail.com>
2025-01-22 07:47:57 +0100

    php extensions

--- modules/nixos/linux/system/systemtools.nix
+++ modules/nixos/linux/system/systemtools.nix
@@ -2,5 +2,13 @@
   environment.systemPackages = with pkgs; [
     lshw
     php
+    php83Extensions.dom
+    php83Extensions.zip
+    php83Extensions.openssl
+    php83Extensions.zlib
+    php83Extensions.gd
+    php83Extensions.pcntl
+    php83Extensions.posix
+    php83Extensions.fpdf
   ];
 }
