d36883260f2925026d5fda93f44e134b3b8083ee
Eelco <eelcovv@gmail.com>
2025-01-22 10:48:04 +0100

    added odbc dependencies

--- modules/home/all/odbc.nix
+++ modules/home/all/odbc.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    spotify
+    libgcc
+    unixODBC
   ];
 }
--- modules/home/profiles/development.nix
+++ modules/home/profiles/development.nix
@@ -2,6 +2,7 @@
   imports = [
     ../all/vscode-server.nix
     ../all/vscode.nix
+    ../all/odbc.nix
     ../all/python.nix
   ];
 }
