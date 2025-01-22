21f5fab2d1cd2a1c605705eb9998156329bd3dd1
Eelco <eelcovv@gmail.com>
2025-01-22 08:59:01 +0100

    use ghostty for a while

--- modules/home/all/wezterm/default.nix
+++ modules/home/all/wezterm/default.nix
@@ -2,6 +2,8 @@
   programs.wezterm = {
     enable = true;
     # https://alexplescan.com/posts/2024/08/10/wezterm/
-    extraConfig = builtins.readFile ./wezterm.lua;
+    extraConfig = builtins.readFile ./config/wezterm/wezterm.lua;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
   };
 }
