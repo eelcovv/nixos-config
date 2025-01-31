42d893fdbaf7009ededc92b0feae8ca04712b997
Eelco van Vlietl <eelco@davelab.nl>
2025-01-31 09:15:56 +0100

    update git config

--- modules/home/all/git.nix
+++ modules/home/all/git.nix
@@ -13,52 +13,68 @@ in
     pre-commit
   ];
 
-  programs.git = {
-    inherit package;
-    difftastic = {
-      enable = true;
-    };
-    enable = true;
-    userName = flake.config.me.fullname;
-    userEmail = flake.config.me.email;
-    aliases = {
-      co = "checkout";
-      ci = "commit";
-      cia = "commit --amend";
-      s = "status";
-      st = "status";
-      b = "branch";
-      # p = "pull --rebase";
-      pu = "push";
-    };
-    iniContent = {
-      # Branch with most recent change comes first
-      branch.sort = "-committerdate";
-      # Remember and auto-resolve merge conflicts
-      # https://git-scm.com/book/en/v2/Git-Tools-Rerere
-      rerere.enabled = true;
-    };
-    ignores = [ "*~" "*.swp" ];
-    lfs.enable = true;
-    extraConfig = {
-      init.defaultBranch = "master"; # Undo breakage due to https://srid.ca/luxury-belief
-      core.editor = "nvim";
-      #protocol.keybase.allow = "always";
-      credential.helper = "store --file ~/.git-credentials";
-      pull.rebase = "false";
-    };
-  };
-
-  programs.lazygit = {
-    enable = true;
-    settings = {
-      # This looks better with the kitty theme.
-      gui.theme = {
-        lightTheme = false;
-        activeBorderColor = [ "white" "bold" ];
-        inactiveBorderColor = [ "white" ];
-        selectedLineBgColor = [ "reverse" "white" ];
+  programs.git =
+    {
+      inherit package;
+      difftastic = {
+        enable = true;
       };
-    };
-  };
+      enable = true;
+      userName = Eelco # flake.config.me.fullname;
+        userEmail = eelcovv@gmail.com # flake.config.me.email;
+      diff = {
+      # tool = vscode
+      tool = "difftastic";
+      options = {
+        difftastic = {
+          # Use the default settings
+        };
+      };
+    }
+      #difftool ={
+      #  vscode = {
+      #    cmd = "code";
+      #    args = ["--wait" "--diff" $LOCAL $REMOTE];
+      #  };
+      #}
+      aliases = {
+  co = "checkout";
+  ci = "commit";
+  cia = "commit --amend";
+  s = "status";
+  st = "status";
+  b = "branch";
+  # p = "pull --rebase";
+  pu = "push";
+};
+iniContent = {
+# Branch with most recent change comes first
+branch.sort = "-committerdate";
+# Remember and auto-resolve merge conflicts
+# https://git-scm.com/book/en/v2/Git-Tools-Rerere
+rerere.enabled = true;
+};
+ignores = [ "*~" "*.swp" ];
+lfs.enable = true;
+extraConfig = {
+init.defaultBranch = "master"; # Undo breakage due to https://srid.ca/luxury-belief
+core.editor = "nvim";
+#protocol.keybase.allow = "always";
+credential.helper = "store --file ~/.git-credentials";
+pull.rebase = "false";
+};
+};
+
+programs.lazygit = {
+enable = true;
+settings = {
+# This looks better with the kitty theme.
+gui.theme = {
+lightTheme = false;
+activeBorderColor = [ "white" "bold" ];
+inactiveBorderColor = [ "white" ];
+selectedLineBgColor = [ "reverse" "white" ];
+};
+};
+};
 }
