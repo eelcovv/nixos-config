8e8e85cbb8c2ca82ebbb379c2fe57041d3b19e11
Eelco van Vlietl <eelco@davelab.nl>
2025-02-03 22:23:40 +0100

    after reformat

--- modules/home/all/git.nix
+++ modules/home/all/git.nix
@@ -31,51 +31,51 @@ in
               # Use the default settings
             };
           };
-        }
-          #difftool ={
-          #  vscode = {
-          #    cmd = "code";
-          #    args = ["--wait" "--diff" $LOCAL $REMOTE];
-          #  };
-          #}
-          aliases = {
-      co = "checkout";
-      ci = "commit";
-      cia = "commit --amend";
-      s = "status";
-      st = "status";
-      b = "branch";
-      # p = "pull --rebase";
-      pu = "push";
+        };
+      #difftool ={
+      #  vscode = {
+      #    cmd = "code";
+      #    args = ["--wait" "--diff" $LOCAL $REMOTE];
+      #  };
+      #}
+      aliases = {
+        co = "checkout";
+        ci = "commit";
+        cia = "commit --amend";
+        s = "status";
+        st = "status";
+        b = "branch";
+        # p = "pull --rebase";
+        pu = "push";
+      };
+      iniContent = {
+        # Branch with most recent change comes first
+        branch.sort = "-committerdate";
+        # Remember and auto-resolve merge conflicts
+        # https://git-scm.com/book/en/v2/Git-Tools-Rerere
+        rerere.enabled = true;
+      };
+      ignores = [ "*~" "*.swp" ];
+      lfs.enable = true;
+      extraConfig = {
+        init.defaultBranch = "master"; # Undo breakage due to https://srid.ca/luxury-belief
+        core.editor = "nvim";
+        #protocol.keybase.allow = "always";
+        credential.helper = "store --file ~/.git-credentials";
+        pull.rebase = "false";
+      };
     };
-  iniContent = {
-    # Branch with most recent change comes first
-    branch.sort = "-committerdate";
-    # Remember and auto-resolve merge conflicts
-    # https://git-scm.com/book/en/v2/Git-Tools-Rerere
-    rerere.enabled = true;
-  };
-  ignores = [ "*~" "*.swp" ];
-  lfs.enable = true;
-  extraConfig = {
-    init.defaultBranch = "master"; # Undo breakage due to https://srid.ca/luxury-belief
-    core.editor = "nvim";
-    #protocol.keybase.allow = "always";
-    credential.helper = "store --file ~/.git-credentials";
-    pull.rebase = "false";
-  };
-};
 
-programs.lazygit = {
-enable = true;
-settings = {
-# This looks better with the kitty theme.
-gui.theme = {
-lightTheme = false;
-activeBorderColor = [ "white" "bold" ];
-inactiveBorderColor = [ "white" ];
-selectedLineBgColor = [ "reverse" "white" ];
-};
-};
-};
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      # This looks better with the kitty theme.
+      gui.theme = {
+        lightTheme = false;
+        activeBorderColor = [ "white" "bold" ];
+        inactiveBorderColor = [ "white" ];
+        selectedLineBgColor = [ "reverse" "white" ];
+      };
+    };
+  };
 }
