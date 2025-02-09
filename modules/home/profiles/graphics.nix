a4c21d4348d87b4f756f402efe8ca0d169dba4a6
Eelco van Vlietl <eelco@davelab.nl>
2025-02-09 12:18:05 +0100

    trying to fix

--- modules/home/profiles/graphics.nix
+++ modules/home/profiles/graphics.nix
@@ -6,8 +6,6 @@
     gimp
     krita
     glmark2
-    callPackage
-    "../all/ptgui.nix"
-    { src = "/nix/store/25hxfzmwkc8nv1k6rhcm3x2ffhz7lbx0-PTGui_13.0.tar.gz"; version = "Pro 13.0"; }
+    (callPackage "../all/ptgui.nix" { src = "/nix/store/25hxfzmwkc8nv1k6rhcm3x2ffhz7lbx0-PTGui_13.0.tar.gz"; version = "Pro 13.0"; })
   ];
 }
