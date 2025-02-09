8978b1d05f0e864953839c2fe8258192ac08923c
Eelco van Vlietl <eelco@davelab.nl>
2025-02-09 10:14:47 +0100

    added new config

--- modules/home/profiles/graphics.nix
+++ modules/home/profiles/graphics.nix
@@ -6,6 +6,5 @@
     gimp
     krita
     glmark2
-    (callPackage ../all/ptgui/ptgui.nix { src = /var/lib/ptgui/PTGui_13.0.tar.gz; version = "Pro 13.0"; })
   ];
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -16,5 +16,6 @@ self: super: {
   actualism-app = inputs.actualism-app.packages.${self.system}.default;
   omnix = inputs.omnix.packages.${self.system}.default;
   git-merge-and-delete = self.callPackage "${packages}/git-merge-and-delete.nix" { };
+  ptgui = self.callPackage "${packages}/ptgui.nix" { };
 }
 
